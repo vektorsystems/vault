a1e48cee8c2a1b30d9b147bff3a2f27661a4db28
Daniel Zamorano <daniel.zamorano.m@gmail.com>
2025-06-26 17:39:20 -0600

    removing custom cors middleware

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -106,7 +106,7 @@ for source in log_sources:
 
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "Vault")
 # if WEBUI_NAME != "Open WebUI":
 #     WEBUI_NAME += " (Open WebUI)"
 
