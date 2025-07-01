0c2f4470806e54c37a0569406986611051e7a665
Daniel Zamorano <daniel.zamorano.m@gmail.com>
2025-07-01 14:46:39 -0600

    rebranding

--- src/lib/components/admin/Settings.svelte
+++ src/lib/components/admin/Settings.svelte
@@ -8,7 +8,7 @@
 	import { getBackendConfig } from '$lib/apis';
 	import Database from './Settings/Database.svelte';
 
-	import General from './Settings/General.svelte';
+	import General from './Settings/VaultGeneral.svelte';
 	import Pipelines from './Settings/Pipelines.svelte';
 	import Audio from './Settings/Audio.svelte';
 	import Images from './Settings/Images.svelte';
